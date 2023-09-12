366840d9e32412526fe28830d7e895669196dac0
Pablo Abratte <pabratte@gmail.com>
2023-09-12 19:46:49 -0300

    fix error and add migrations

--- quotes/views.py
+++ quotes/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from quotes.models import Item
 
 # Create your views here.
 def main(request):
-    return render(request, 'quotes/main.html')
+    context = {
+        "items": Item.objects.all()
+    }
+    return render(request, 'quotes/main.html', context)
