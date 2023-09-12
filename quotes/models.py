e412bef64e59e7727e94b5c6d2709711d00c4c67
Pablo Abratte <pabratte@gmail.com>
2023-09-12 19:43:38 -0300

    add models

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+    provider = models.ForeignKey('Provider', null=True, on_delete=models.CASCADE)
+
+class Provider(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=30)
